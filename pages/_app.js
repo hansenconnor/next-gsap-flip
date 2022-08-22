7aa218b26479813d3307f939e8ecbbc3f79e0c04
Connor H <connor@hvnsen.com>
2022-08-22 15:20:44 -0400

    initial commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,24 @@
-import '../styles/globals.css'
+import React, { useState } from "react";
+import "../styles/globals.scss";
+import { FlipContext, FlipProvider } from "../context/flip-context";
+import { gsap } from "gsap";
+import { Flip } from "gsap/dist/Flip";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  gsap.registerPlugin(Flip);
+
+  const [lastState, setLastState] = useState(null);
+
+  const dispatchFlipStateEvent = (payload) => {
+    console.log(payload);
+    setLastState(payload);
+  };
+
+  return (
+    <FlipContext.Provider value={{ lastState, dispatchFlipStateEvent }}>
+      <Component {...pageProps} />
+    </FlipContext.Provider>
+  );
 }
 
-export default MyApp
+export default MyApp;
